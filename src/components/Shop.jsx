eac456fd4eac7c4f0e6d16f836ae3e1b6acfaa4b
tuhin <tuhin153695@gmail.com>
2023-03-31 21:16:03 +0600

    order summary added

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from './Product/Product';
 
-
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
 
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
 
     return (
         <div className='shop-container'>
@@ -19,11 +25,13 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h4>Order <summary></summary></h4>
+                <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
