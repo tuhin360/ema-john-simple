92b5e17a21a602ff2796c79202eddd882778a034
tuhin <tuhin153695@gmail.com>
2023-04-09 13:54:47 +0600

    delete cart added

---
+++ src/loader/cardProductsloader.js
@@ -0,0 +1,26 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
+ const cartProductsLoader = async () =>{
+    const loadedProducts = await fetch('products.json');
+    const products = await loadedProducts.json();
+
+    // if cart data is in database, you have to use async await
+    const storedCart = getShoppingCart();
+    console.log(storedCart);
+
+    const savedCart = [];
+
+    for(const id in storedCart){
+        const addedProduct = products.find(pd => pd.id === id);
+        if(addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }      
+    }
+
+ 
+    return savedCart;
+ }
+
+ export default cartProductsLoader;
